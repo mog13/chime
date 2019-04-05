a846280d11cb11e73e17119246dc2698816e738f
morgan <mog13@live.com>
2019-04-05 15:47:14 +0100

    added initial PlayNote function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-export class Chime {
+ class Chime {
 	
     constructor() {
+	this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+	this.default = {
+		volume:0.5,
+		freq:500,
+		duration:1,
+		type:"sine"
+	}
+    }
 
+    PlayNote(frequency,duration,volume,type) {
+	if(frequency === undefined) frequency = this.default.freq;
+	if(duration === undefined) duration = this.default.duration;
+	if(volume === undefined) volume = this.default.volume;
+	if(type === undefined) type = this.default.type;
+
+	let osc = this.ctx.createOscillator();
+	let gain = this.ctx.createGain();
+	gain.gain.setValueAtTime(volume|0.5,this.ctx.currentTime);
+	osc.type = type;
+	osc.frequency.setValueAtTime(frequency,this.ctx.currentTime);
+	osc.connect(gain);
+	gain.connect(this.ctx.destination);
+	osc.start();
+	osc.stop(this.ctx.currentTime + (duration*1000));
     }
 }
+
+module.exports = new Chime();
